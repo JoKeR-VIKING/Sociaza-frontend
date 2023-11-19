f3a35435fcc96e06fc3e75b8dbb31152f48ee164
Pratham Vasani <73855805+JoKeR-VIKING@users.noreply.github.com>
2023-11-19 18:16:31 +0530

    Update Profile.js

--- src/pages/social/profile/Profile.js
+++ src/pages/social/profile/Profile.js
@@ -125,6 +125,7 @@ export const Profile = () => {
 
 		try {
 			const response = await userService.getUserProfileByUsername(username, searchParams.get('id'), searchParams.get('uId'));
+			console.log(searchParams);
 			setUser(response?.data?.user);
 			setUserProfileData(response?.data);
 			if (response?.data?.user?.bgImageVersion)
